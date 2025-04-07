4cc07fa31b3e956e5c6e1576342e1c77088ddbc1
Beatriz <beatriz.giovanna@gmail.com>
2025-04-07 00:58:53 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+double milhas = 0.0, km = 0;
+
+Console.WriteLine("Digite a medida em Milhas:");
+milhas = Convert.ToDouble(Console.ReadLine()!);
+
+km = milhas *  1.609;
+
+Console.Write($"O valor convertido é: {km} Km");
